cffa6006e2941ed17869d57caab159d1aa7eacee
Rafi Fitzmaurice <rafi.fitzm@gmail.com>
2025-11-19 16:32:16 +0000

    Implemented new restaurant user story

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,11 @@ Rails.application.routes.draw do
   # A visitor can see the list of all restaurants.
   get "restaurants", to: "restaurants#index"
 
+  # A visitor can add a new restaurant, and be redirected to the show page of that restaurant
+  get "restaurants/new", to: "restaurants#new"
+  post "restaurants", to: "restaurants#create"
+
   # A visitor can see the details of a restaurant, with all the reviews related to that restaurant
   get "restaurants/:id", to: "restaurants#show", as: :restaurant
+
 end
